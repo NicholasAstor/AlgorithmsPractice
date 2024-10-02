8068ed6367918d8b96174b26cd43dc6b0e5b3aec
Nicholas Ástor Barbosa Navarro <nicholas.barbosa@edu.pucrs.br>
2024-10-02 16:02:50 -0300

    feat(vetor): implementa métodos remove e find
    Adicionei o método remove para eliminar todas as ocorrências de um item do vetor
    e o método find para retornar o índice de um item ou -1 se não for encontrado.
    
    docs: adicionei comentários explicativos em todo o código para melhor clareza.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,4 +1,7 @@
 package com.example;
+/*Vou utilizar a classe Main para testar o funcionamento do algoritmo que estou criando
+ *assim consigo de forma mais eficaz determinar se necessita de algum ajuste.
+ */
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +11,9 @@ public class Main {
         vetor.push(2);
         vetor.push(3);
         vetor.push(4);
-        vetor.push(5);
+        vetor.push(3);
+        vetor.push(3);
+        vetor.push(3);
         //System.out.println(vetor.isEmpty());
         //System.out.println(vetor.size());
         //System.out.println(vetor.capacity());
@@ -17,8 +22,18 @@ public class Main {
         //System.out.println(vetor.at(1));
         //System.out.println(vetor.at(2));
         //System.out.println(vetor.at(3));
-        vetor.delete(2);
-        System.out.println("");
+        //vetor.delete(4);
+        //vetor.delete(6);
+        //vetor.remove(3);
+        vetor.insert(6,6);
+        vetor.insert(2,22);
+        vetor.insert(1,61);
+        vetor.pop();
+        vetor.pop();
+        vetor.pop();
+        vetor.pop();
+        vetor.pop();
+        vetor.pop();
         for(int i = 0; i<vetor.size(); i++){
             System.out.println(vetor.at(i));
         }
--- src/main/java/com/example/Vetor.java
+++ src/main/java/com/example/Vetor.java
@@ -6,31 +6,47 @@ public class Vetor {
 
     private int[] arr;
     private int size;
-
+    
+    /*Construtor sem parâmetros, inicia um vetor de tamanho 10 
+    sem elementos por isso size começa zerado*/
     public Vetor(){
         this.size = 0;
         this.arr = new int[10]; 
     }
 
+    /*Construtor com o parâmetro de tamanho do vetor a ser criado
+     *também inicia com size zerado por não tem nenhum elemento
+     dentro do vetor ainda
+     */
     public Vetor(int n){
         this.size = 0;
         this.arr = new int[n];
     }
 
+    /*Método criado para termos um vetor com redimensionamento
+     *automático. Aumenta em dobro o tamanho do nosso vetor
+     */
     private void resize(){
         int[] newArr = new int[arr.length * 2];
         System.arraycopy(arr, 0, newArr, 0, arr.length);
         arr = newArr;        
     }
 
+    /*Retorna a quantidade de elementos que temos atualmente dentro
+     *do nosso vetor
+     */
     public int size(){
         return size;
     }
 
+    /*Retorna a capacidade que o vetor possui atualmente ela pode sim
+     * aumentar devido ao método resize()
+     */
     public int capacity(){
         return arr.length;
     }
 
+    /*Retorna true caso nosso vetor não tenha elementos */
     public boolean isEmpty(){
         if(size == 0){
             return true;
@@ -40,14 +56,16 @@ public class Vetor {
         }
     }
 
+    /*Retorna o conteúdo de determinado index(parâmetro) */
     public int at(int index){
-        if(index>arr.length){
+        if(index>=size || index<0){
             throw new ArrayIndexOutOfBoundsException();
         }
 
         return arr[index];
     }
 
+    /*Nos permite colocar elementos sempre ao final do vetor  */
     public void push(int n){
         if(size == arr.length){
             resize();
@@ -56,8 +74,11 @@ public class Vetor {
         arr[size++] = n;
     }
 
+    /*Método criado para conseguir colocar algum n(parâmetro) em um
+     * index(parâmetro) específico
+     */
     public void insert(int index, int n){
-        if(index>arr.length){
+        if(index>=size || index<0){
             throw new ArrayIndexOutOfBoundsException();
         }
         if(size == arr.length){
@@ -69,20 +90,56 @@ public class Vetor {
         size++;
     }
     
+    /*Retorna o último elemento do vetor e retira o mesmo */
     public int pop(){
-        int n = arr[size-1];
+        if(size==0){
+            throw new ArrayIndexOutOfBoundsException("O vetor está vazio.");
+        }
+
+        /*int n = arr[size-1];
         int[] tempArr = new int[arr.length];
         System.arraycopy(arr, 0, tempArr, 0, size - 1);
-        arr = tempArr;
-        return n;
+        arr = tempArr;*/
+
+
+        return arr[--size];
     }
 
+    /*Remove o elemento de determinado index(parâmetro) dentro
+     * do Vetor
+     */
     public void delete(int index){
-        if(index>arr.length){
+        if(index>=size || index<0){
             throw new ArrayIndexOutOfBoundsException();
         }
 
         System.arraycopy(arr, index + 1, arr, index, (size-1) - index);
         size--;
     }
+
+    /*Remove um determinado item dentro do Vetor, funciona mesmo
+     * se o item tiver mais de uma ocorrência 
+     */
+    public void remove(int item){
+        for(int i = 0; i<size; i++){
+            if(arr[i]==item){
+                delete(i);
+                i--;
+            }
+        }
+    }
+
+    /*Retorna o valor do index onde o item(parâmetro) está, se não tiver nenhum item
+     * retorna -1
+     */
+    public int find(int item){
+        for(int i=0; i<size; i++){
+            if(arr[i]==item){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 }
